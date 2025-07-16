7db5638ebf3c04335e6609bdb5bfa084fd33be74
Debasish Debnath <ddebnath241@gmail.com>
2025-07-16 18:42:34 +0530

    first commit

---
+++ backend/user/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+
+dotenv.config();
+const app = express();
+connectDb();
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
