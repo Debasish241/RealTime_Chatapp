afd244871faa40588c7b20b1fb176e18c774a149
Debasish Debnath <ddebnath241@gmail.com>
2025-07-16 18:48:56 +0530

    Add Redis dependencies and initialize Redis client

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
-
+import { createClient } from "redis";
 dotenv.config();
 const app = express();
 connectDb();
+
+export const redisClient = createClient();
 const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
+// 31:45
