807dade7166ce7b7761378ce064b1770db0a4f68
Debasish Debnath <91756891+Debasish241@users.noreply.github.com>
2025-08-01 15:08:43 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+timport type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+eslint:{
+  ignoreDuringBuilds: true,
+}
 };
 
 export default nextConfig;
